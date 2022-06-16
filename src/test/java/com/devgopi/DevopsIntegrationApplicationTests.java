e4f2268559720e326e7bd11eaa55badbbd8a3d60
Dev-Gopi <gopinath@gmail.com>
2022-06-16 13:06:08 +0530

    this is devops automation using jenkins

---
+++ src/test/java/com/devgopi/DevopsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devgopi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
